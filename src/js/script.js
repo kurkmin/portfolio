d26e3f9bfd5e3bf7c9df5fe010adf5ae614ebd7e
kurkmin <kurkmin@gmail.com>
2026-01-29 16:42:29 +0000

    dom content loaded added

--- src/js/script.js
+++ src/js/script.js
@@ -4,12 +4,13 @@ document.addEventListener('click', () => {
     console.log("clcked!");
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const visCo2FlyLink = document.querySelector('#vis-co2-fly-link');
+    visCo2FlyLink.innerText = "vis-co2-fly.com";
+});
+
 window.onload = () => {
     const mainHeader = document.querySelector('#title');
     mainHeader.innerText = "deployment successful!";
 };
 
-window.onload = () => {
-    const visCo2FlyLink = document.querySelector('#vis-co2-fly-link')
-    visCo2FlyLink.innerText = "vis-co2-fly.com"
-}
