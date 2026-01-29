d18b4d0d844b61570b23ad35d29506c470ec0d9e
kurkmin <kurkmin@gmail.com>
2026-01-29 16:32:06 +0000

    link added

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,8 @@ window.onload = () => {
     const mainHeader = document.querySelector('#title');
     mainHeader.innerText = "deployment successful!";
 };
+
+window.onload = () => {
+    const visCo2FlyLink = document.querySelector('#vis-co2-fly-link')
+    visCo2FlyLink.innerText = "vis-co2-fly.com"
+}
