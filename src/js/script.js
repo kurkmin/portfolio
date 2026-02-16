1d76c20dfdf9f488f93f2d07094ae7b1233e41d6
Sumin Oh <kurkmin@gmail.com>
2026-02-16 15:46:23 +0900

    testing message added

--- src/js/script.js
+++ src/js/script.js
@@ -4,13 +4,13 @@ document.addEventListener('click', () => {
     console.log("clcked!");
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const visCo2FlyLink = document.querySelector('#vis-co2-fly-link');
-    visCo2FlyLink.innerText = "vis-co2-fly.com";
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     const visCo2FlyLink = document.querySelector('#vis-co2-fly-link');
+//     visCo2FlyLink.innerText = "vis-co2-fly.com";
+// });
 
 window.onload = () => {
     const mainHeader = document.querySelector('#title');
-    mainHeader.innerText = "deployment successful!";
+    mainHeader.innerText = "🚧 Under Construction!";
 };
 
